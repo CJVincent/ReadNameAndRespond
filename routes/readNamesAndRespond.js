da4f0242d9430a09a5e38c92f9c2d686e1f76f27
CJVincent <christophvincent@gmail.com>
2017-10-31 13:18:27 -0700

    Working

--- routes/readNamesAndRespond.js
+++ routes/readNamesAndRespond.js
@@ -1,10 +1,13 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/', bodyParser, function(req, res, next) {
-    res.render('index', { name: req.body.name });
+router.get('/', function(req, res, next) {
+    res.send("Welcome");
+});
+router.post('/',function(req, res) {
+    var postName = req.body.name;
+    res.render('index', { name: postName });
 });
 
 module.exports = router;

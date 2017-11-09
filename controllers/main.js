dd84d8fa3cb6ce897004a99812d3c2c34effb820
CJVincent <christophvincent@gmail.com>
2017-11-09 10:52:19 -0800

    Nov 9 Exercise w/ Controllers

---
+++ controllers/main.js
@@ -0,0 +1,22 @@
+var mongodb = require('mongodb').MongoClient;
+var mongoDBURI = process.env.MONGODB_URI ||'mongodb://CJV:doritos61@ds153015.mlab.com:53015/heroku_qttgdqm3';
+module.exports.index = function(req, res, next) {
+    res.render('index', { title: 'Express' });
+};
+
+module.exports.getDB = function(req, res, next) {
+    mongodb.connect(mongoDBURI, function(err, db) {
+        if (err) throw err;
+
+        var dbColl = db.collection('ORDERS');
+        dbColl.find({ }).toArray(function(err, docs) {
+            if (err) throw err;
+            console.log(docs);
+            res.render('db', { data: JSON.stringify(docs)});
+        });
+
+        db.close(function (err) {
+            if (err) throw err;
+        });
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var mongodb = require('mongodb').MongoClient;
-var mongoDBURI = process.env.MONGODB_URI ||'mongodb://CJV:doritos61@ds153015.mlab.com:53015/heroku_qttgdqm3';
+
+var ControllerMain = require('../controllers/main');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ControllerMain.index);
 
-router.get('/db', function(req, res, next) {
-    mongodb.connect(mongoDBURI, function(err, db) {
-        if (err) throw err;
-
-        var dbColl = db.collection('ORDERS');
-        dbColl.find({ }).toArray(function(err, docs) {
-            if (err) throw err;
-            console.log(docs);
-            res.render('db', { data: JSON.stringify(docs)});
-        });
-
-        db.close(function (err) {
-            if (err) throw err;
-        });
-    });
-});
+router.get('/db', ControllerMain.getDB);
 
 module.exports = router;
